cf3348d9a76928bbbee21205cfa25033e3b5c537
Brian Nguyen <briannguyen.1992@gmail.com>
2025-07-15 19:04:28 +1000

    added 2nd method for more complex strings with special characters

--- StringFormatter.cs
+++ StringFormatter.cs
@@ -15,11 +15,37 @@ namespace PointsBet_Backend_Online_Code_Test
     public class StringFormatter
     {
         private const string Separator = ",";
+        private const string Quote = "\"";
         
-        public static string ToCommaSeparatedList(string[] items)
+        private const string StringFormat = "{0}{1}{0}";
+        
+        public static string ToCommaSeparatedListSimpleStrings(string[] items)
         {
-            // refactored to use join as this handles array items > 1 correctly
+            // refactored to use join as this handles array items > 1 correctly for simple arrays
             return items == null || items?.Length == 0 ? string.Empty : string.Join(Separator, items);
+        }
+        
+        public static string ToCommaSepatatedListComplexStrings(string[] items, string quote)
+        {
+            // refactor for more complex arrays with quote marks
+            StringBuilder qry = new StringBuilder();
+
+            if (items == null || items?.Length == 0)
+            {
+                return qry.ToString();
+            }
+            
+            for (int i = 0; i < items.Length; i++)
+            {
+                qry.Append(string.Format(StringFormat, quote, items[i]));
+
+                if (i < items.Length - 1)
+                {
+                    qry.Append(Separator);
+                }
+            }
+
+            return qry.ToString();
         }
     }
 }
